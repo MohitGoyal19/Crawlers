779f26e56616038be17af9a06d7bada8070264be
MohitGoyal19 <gm.mohitgoyal@gmail.com>
2020-06-14 13:49:28 +0000

    data

--- yellowpages za/yellowpages.py
+++ yellowpages za/yellowpages.py
@@ -25,7 +25,7 @@ def get_data(link, cat):
 
     page = bs(response.text, 'html5lib')
 
-    data = json.loads(page.find('script', {'type': 'application/ld+json'}).text) if page.find('script', {'type': 'application/ld+json'}).text else ''
+    data = json.loads(page.find('script', {'type': 'application/ld+json'}).text) if page.find('script', {'type': 'application/ld+json'}) else ''
     if not data:
         return [None]*25
 
