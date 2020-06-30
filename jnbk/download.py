9cd6fc123fbf3daabcff32ce4f371f6140f9f058
Mohit Goyal <43956903+MohitGoyal19@users.noreply.github.com>
2020-07-01 00:24:05 +0530

    Update download.py

--- jnbk/download.py
+++ jnbk/download.py
@@ -1,3 +1,3 @@
 import pandas as pd
 
-pd.read_csv('items_jnbk_2.csv').drop_duplicates().to_excel('jnbk.xlsx', index=None)
+pd.read_csv('items_jnbk_2.csv', low_memory=False).drop_duplicates().to_excel('jnbk.xlsx', index=None)
